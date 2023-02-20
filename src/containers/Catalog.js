1acbd346fd6e420b87ca94c7ae3283d3280333bd
Roksolana Mazurak <roksamazurak@gmail.com>
2023-02-20 19:09:53 +0200

    connect to rest api

--- src/containers/Catalog.js
+++ src/containers/Catalog.js
@@ -1,14 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CardItem from "../components/CardItem";
 import FilterBar from "../components/FilterBar";
-import books from "./books";
 import Input from "../components/Input";
+import { getAllItems, getFilteredItems } from "../api/api";
+import Loader from "../components/Loader";
 
 function Catalog() {
-  const [items, setItems] = useState(books);
+  const [items, setItems] = useState([]);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    getAllItems().then((res) => {
+      setItems(res);
+      setLoader(false);
+    });
+  }, []);
 
   const inputChangeHandler = (searchingText) => {
-    setItems(books);
     if (searchingText.length >= 1) {
       setItems(
         items.filter(
@@ -22,7 +30,9 @@ function Catalog() {
         )
       );
     } else {
-      setItems(books);
+      getAllItems().then((res) => {
+        setItems(res);
+      });
     }
   };
 
@@ -31,40 +41,10 @@ function Catalog() {
       ...filters,
     };
 
-    let filteredItems = [...books];
-
-    if (appliedFilters.price !== null) {
-      if (
-        appliedFilters.price.toString().toLowerCase() === "from high to low"
-      ) {
-        filteredItems = filteredItems.sort((a, b) =>
-          a.price < b.price ? 1 : -1
-        );
-      } else if (
-        appliedFilters.price.toString().toLowerCase() === "from low to high"
-      ) {
-        filteredItems = filteredItems.sort((a, b) =>
-          a.price > b.price ? 1 : -1
-        );
-      }
-    }
-
-    if (appliedFilters.availability !== null) {
-      filteredItems = filteredItems.filter(
-        (item) =>
-          item.availability.toLowerCase() ===
-          appliedFilters.availability.toString().toLowerCase()
-      );
-    }
-
-    if (appliedFilters.genre !== null) {
-      filteredItems = filteredItems.filter(
-        (item) =>
-          item.genre.toLowerCase() ===
-          appliedFilters.genre.toString().toLowerCase()
-      );
-    }
-    setItems(filteredItems);
+    getFilteredItems(appliedFilters).then((res) => {
+      setItems(res);
+      setLoader(false);
+    });
   };
 
   return (
@@ -72,6 +52,7 @@ function Catalog() {
       <Input onChangeInput={inputChangeHandler} />
       <FilterBar onChangeFilter={filterChangeHandler}></FilterBar>
       <div className="container">
+        {loader && <Loader />}
         {items.map(
           ({ id, title, author, image, genre, price, availability }, idx) => (
             <CardItem
--- src/containers/ItemPage.js
+++ src/containers/ItemPage.js
@@ -1,48 +1,65 @@
-import React, { useState } from "react";
-import { useParams } from "react-router-dom";
-import books from "./books";
+import React, { useState, useEffect } from "react";
 import "./ItemPage.css";
+import { useParams } from "react-router-dom";
+import { getItemById } from "../api/api";
+import Loader from "../components/Loader";
 
 function ItemPage() {
-  const [items, setItems] = useState(books);
+  const [item, setItem] = useState([]);
   const { id } = useParams();
-  const item = items.find((item) => item.id == id);
+  const [loader, setLoader] = useState(true);
+
+  useEffect(() => {
+    getItemById(id).then((res) => {
+      setItem(res);
+      setLoader(false);
+    });
+  }, [id]);
 
   return (
-    <div className="item-container">
-      <div className="item-image-container">
-        <img src={item.image} alt="Book cover" className="item-image" />
-      </div>
-      <div>
-        <div className="tag-wrapper">
-          <div className="tag-avaible">{item.availability}</div>
-          <div className="tag-genre">{item.genre}</div>
+    <>
+      {loader && <Loader />}
+      <div className="item-container">
+        <div
+          className="item-image-container"
+          style={{ display: !loader ? "block" : "none" }}
+        >
+          <img src={item.image} alt="Book cover" className="item-image" />
         </div>
-        <h1 className="item-title">{item.title}</h1>
-        <h3>by {item.author}</h3>
-        <p className="item-paragraph">{item.description}</p>
-        <input
-          className="item-quantity-input"
-          placeholder="Enter quantity"
-          type="number"
-          min="1"
-        ></input>
-        <select className="item-select">
-          <option value="0">Special bookmark</option>
-          <option value="1">Add bookmark</option>
-          <option value="2">None</option>
-        </select>
-        <div className="price-wrapper">
-          <h2>Price: {item.price} $</h2>
-          <div className="item-buttons">
-            <button className="item-button">
-              <a href="/catalog">Go back</a>
-            </button>
-            <button className="item-add-button">Add to cart</button>
+        <div style={{ display: !loader ? "block" : "none" }}>
+          <div className="tag-wrapper">
+            <div className="tag-avaible">{item.availability}</div>
+            <div className="tag-genre">{item.genre}</div>
+          </div>
+          <h1 className="item-title">{item.title}</h1>
+          <h3>by {item.author}</h3>
+          <p className="item-paragraph">{item.description}</p>
+          <input
+            className="item-quantity-input"
+            placeholder="Enter quantity"
+            type="number"
+            min="1"
+          ></input>
+          <select className="item-select">
+            <option value="0">Special bookmark</option>
+            <option value="1">Add bookmark</option>
+            <option value="2">None</option>
+          </select>
+          <div className="price-wrapper">
+            <h2>Price: {item.price} $</h2>
+            <div className="item-buttons">
+              <button className="item-button">Go back</button>
+              <button
+                disabled={item.availability === "not available" ? true : false}
+                className="item-add-button"
+              >
+                Add to cart
+              </button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 export default ItemPage;
